0b8e8c8203124cd309cbd3159c3164a9e3cd96db
Palak P Shah <dabbadolcha@gmail.com>
2021-10-25 23:54:55 +0530

    circleci driver path 7

--- GUIAutomatedTests/features/steps/environment.py
+++ GUIAutomatedTests/features/steps/environment.py
@@ -9,21 +9,22 @@ options = Options()
 options.headless = True
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-options.binary_location="GUIAutomatedTests/ChromeExe/linux/chromedriver_linux64/chromedriver"
+options.binary_location='GUIAutomatedTests/ChromeExe/linux/chromedriver_linux64/chromedriver'
+print("Binary Location",options.binary_location)
 options.add_argument('--start-maximized')
 # options.add_argument("--disable-infobars")
-options.add_argument("--disable-notifications")
+options.add_argument('--disable-notifications')
 # added these 3 lines for circleci linux
 options.add_argument('--headless')
 
 # added these 3 lines above for circleci linux
 #options.add_argument("--headless")
-options.add_argument("--window-size=1920x1080")
+options.add_argument('--window-size=1920x1080')
 user_agent = \
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 PTST/1.0'
 options.add_argument('user-agent={0}'.format(user_agent))
 # use this code below to execute headless state
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options,executable_path='GUIAutomatedTests/ChromeExe/linux/chromedriver_linux64/chromedriver')
 #driver = webdriver.Chrome("/home/circleci/.wdm/drivers/chromedriver/linux64/95.0.4638.17",options=options)
 # service = Service("C:\\exe installer\\chrome driver\\chromedriver_win32\\chromedriver.exe")
 
